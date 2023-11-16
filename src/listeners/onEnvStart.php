b877c812593d3077f6cec4abfa7680999c5b26c7
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-11-16 18:19:40 +0100

    feat: OnEnvStart

--- src/listeners/onEnvStart.php
+++ src/listeners/onEnvStart.php
@@ -2,26 +2,16 @@
 use Data\Counter;
 use Lenra\App\Collection;
 
-error_log("onEnvStart.php called");
-error_log("onEnvStart.php: creating counter for user 'global'");
-
-var_error_log($request);
-
 $user = "global";
 /**
  * @var Collection
  */
 $counterColl = $request->api->data()->coll(Counter::class);
 $counters = $counterColl->find([ "user" => $user ], [])->wait();
+
+error_log("onEnvStart.php - " . count($counters) . " counters found for user $user");
+
 if (count($counters) == 0) {
     $counterColl->createDoc(new Counter($user, 0))->wait();
-}
-
-function var_error_log( $object=null ){
-    ob_start();                    // start buffer capture
-    var_dump( $object );           // dump the values
-    $contents = ob_get_contents(); // put the buffer into a variable
-    ob_end_clean();                // end capture
-    error_log( $contents );        // log contents of the result of var_dump( $object )
 }
 ?>
