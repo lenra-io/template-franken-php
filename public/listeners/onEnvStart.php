b4aaa38ae5368f8a925f33bc32d3c98c1ed995ad
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-11-17 16:54:48 +0100

    refactor: Better lenra.yml file

--- public/listeners/onEnvStart.php
+++ public/listeners/onEnvStart.php
@@ -1,4 +1,5 @@
 <?php
+
 use Data\Counter;
 use Lenra\App\Collection;
 
@@ -7,11 +8,13 @@ $user = "global";
  * @var Collection
  */
 $counterColl = $request->api->data()->coll(Counter::class);
-$counters = $counterColl->find([ "user" => $user ], [])->wait();
+$counters = $counterColl->find([ "user" => $user ])->wait();
 
 error_log("onEnvStart.php - " . count($counters) . " counters found for user $user");
 
 if (count($counters) == 0) {
+    error_log("onEnvStart.php - creating counter for user $user");
     $counterColl->createDoc(new Counter($user, 0))->wait();
+    error_log("onEnvStart.php - counter created for user $user");
 }
 ?>
