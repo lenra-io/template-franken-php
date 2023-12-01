6888e3dc0127ade1353507d1481c696be639510e
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-12-01 11:45:57 +0000

    feat: Use acpu cache

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 
+error_log(hrtime(true) . " - index.php");
+require_once __DIR__ . '/../vendor/autoload.php';
+error_log(hrtime(true) . " - after autoload");
 \Lenra\App\Runner::handleServerRequest();
+error_log(hrtime(true) . " - request handled");
